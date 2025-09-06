fe256baf5e8e40b407f19c2ee0f7522de0d1af13
aspal212 <cusmin212@gmail.com>
2025-09-06 15:50:17 +0000

    add

--- script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-
-// Contoh interaktif sederhana: klik header untuk alert
-const header = document.querySelector('header h1');
-header.addEventListener('click', () => {
-    alert('Selamat datang di website sederhana!');
-});
