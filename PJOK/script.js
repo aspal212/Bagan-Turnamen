35ac6e5f3058904252bfd4a5a523583aafc85c86
aspal212 <cusmin212@gmail.com>
2025-09-06 15:02:38 +0000

    add

---
+++ PJOK/script.js
@@ -0,0 +1,7 @@
+// script.js
+
+// Contoh interaktif sederhana: klik header untuk alert
+const header = document.querySelector('header h1');
+header.addEventListener('click', () => {
+    alert('Selamat datang di website sederhana!');
+});
